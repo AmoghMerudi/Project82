65680f34a633b58ff68d1b76ec4f31dc7cdbeeea
AmoghMerudi <73833979+AmoghMerudi@users.noreply.github.com>
2022-05-16 15:07:05 +0530

    Add files via upload

---
+++ Spectagram/spectagram/screens/CreatePost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+export default class CreatePost extends React. Component{
+  render(){
+    return(
+      <View>
+        <Text>CreatePost</Text>
+      </View>
+    );
+  }
+} 
